c71ab6446a895ee50d447a42c5b9570e476a9f8c
jjklee <jjlee720@gmail.com>
2019-10-24 17:59:30 -0700

    init

---
+++ build/precache-manifest.0c80dfc7f17ffe9e47a1ffabd5c82fcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eada65cb9f2b3f01f8efa9333f846ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43956436095c94586d22",
+    "url": "/static/css/main.138adaa0.chunk.css"
+  },
+  {
+    "revision": "998cf2967441db9136e4",
+    "url": "/static/js/2.8bbd87c5.chunk.js"
+  },
+  {
+    "revision": "43956436095c94586d22",
+    "url": "/static/js/main.c2e261fb.chunk.js"
+  },
+  {
+    "revision": "1262d10a3d622c71ec2a",
+    "url": "/static/js/runtime-main.cea0d507.js"
+  },
+  {
+    "revision": "3d30f44fa621b3cf813534cbc27682a9",
+    "url": "/static/media/background.3d30f44f.jpeg"
+  }
+]);
