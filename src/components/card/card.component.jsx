51e2d3a875c3cbbc443cb9cb7ff1f2e0af95e104
jjklee <jjlee720@gmail.com>
2019-10-24 18:03:45 -0700

    [visual] moves arrows from card to column component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import '../card/card.style.css';
+import React from "react";
+import "../card/card.style.css";
 
-export const Card = ({ card, colInd, cardInd, moveRight, moveLeft }) => (
-  <div className="card">
-    {colInd > 0 ? <button className="left-arrow" onClick={() => moveLeft(colInd, cardInd)}/> : null}
-    <span>{card}</span>
-    {colInd < 3 ? <button className="right-arrow" onClick={() => moveRight(colInd, cardInd)}/> : null}
-  </div>
-)
+export const Card = ({ card, handleRemoveCard, colInd, cardInd }) => (
+	<div className="card">
+		<span>{card}</span>
+		<span
+			className="delete-card"
+			onClick={() => handleRemoveCard(colInd, cardInd)}
+		/>
+	</div>
+);
--- src/components/column/column.component.jsx
+++ src/components/column/column.component.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
-import { Card } from '../card/card.component.jsx';
-import './column.style.css';
+import React from "react";
+import { Card } from "../card/card.component.jsx";
+import "./column.style.css";
 
-export const Column = (props) => (
-  <div className="single-column">
-    <h3 className="title" >{props.column.title}</h3>
-    {props.column.cards.map((card, i) => (
-      <Card card={card} key={i} colInd={props.colInd} cardInd={i} moveRight={props.moveRight} moveLeft={props.moveLeft}/>
-    ))}
-    <button className="add" id={props.position} onClick={props.handleClick}>Add a card</button>
-  </div>
-)
+export const Column = ({
+	column,
+	handleAddCard,
+	handleRemoveCard,
+	moveLeft,
+	moveRight,
+	colInd
+}) => (
+	<div className="single-column">
+		<p className="title">{column.title}</p>
+		{column.cards.map((card, i) => (
+			<div className="card-container" key={i}>
+				{colInd > 0 ? (
+					<button className="left-arrow" onClick={() => moveLeft(colInd, i)} />
+				) : (
+					<button className="no-arrow" />
+				)}
+				<Card
+					card={card}
+					handleRemoveCard={handleRemoveCard}
+					colInd={colInd}
+					cardInd={i}
+				/>
+				{colInd < 3 ? (
+					<button
+						className="right-arrow"
+						onClick={() => moveRight(colInd, i)}
+					/>
+				) : (
+					<button className="no-arrow" />
+				)}
+			</div>
+		))}
+		<button className="add" id={colInd} onClick={handleAddCard}>
+			Add another card
+		</button>
+	</div>
+);
