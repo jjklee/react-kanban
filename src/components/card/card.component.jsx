9d9e6c356460f7b35a15a8b8ab5a5454594e1844
jjklee <jjlee720@gmail.com>
2019-10-25 19:34:07 -0700

    [feat] adds edit button

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,9 +4,9 @@ import "../card/card.style.css";
 export const Card = ({ card, handleRemoveCard, colInd, cardInd }) => (
 	<div className="card">
 		<span>{card}</span>
-		<span
-			className="delete-card"
-			onClick={() => handleRemoveCard(colInd, cardInd)}
-		/>
+		<div className="card-tools">
+			<button onClick={() => handleRemoveCard(colInd, cardInd)}>delete</button>
+			<button>edit</button>
+		</div>
 	</div>
 );
