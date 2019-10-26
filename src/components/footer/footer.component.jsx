5d624fcee0b8b7b230c1e4e7d7c8fa519eb1c5bb
jjklee <jjlee720@gmail.com>
2019-10-25 19:33:09 -0700

    [visual] removes footer

--- src/components/footer/footer.component.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './footer.style.css';
-
-export const Footer = () =>
-  <div className="footer">
-    <a className="name" href="https://jjklee.com">☺ jjklee.com</a>
-  </div>
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
-import './navbar.style.css';
+import React from "react";
+import { SearchResults } from "../search-results/search-results.component";
+import logo from "../../assets/favicon.ico";
+import "./navbar.style.css";
 
-export const NavBar = () =>
-  <div className="nav">
-    react-kanban
-  </div>
+export const NavBar = ({ handleSearch, searchResults, search }) => (
+	<div className="nav">
+		<input
+			className="searchbar"
+			type="search"
+			placeholder="Search for a card..."
+			onChange={handleSearch}
+		/>
+		{searchResults.length > 0 && !(search.length < 3) ? (
+			<SearchResults searchResults={searchResults} />
+		) : null}
+		<span className="header">react-kanban</span>
+		<div className="portfolio">
+			<a href="https://jjklee.com">jjklee.com</a>
+			<img src={logo} alt="logo" className="logo" />
+		</div>
+	</div>
+);
