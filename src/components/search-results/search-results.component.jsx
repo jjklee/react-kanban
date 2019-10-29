c9807445d5a8ece459aa12d5d8f3adb5741ca181
jjklee <jjlee720@gmail.com>
2019-10-28 23:24:00 -0700

    [feat] shows results for no cards

--- src/components/search-results/search-results.component.jsx
+++ src/components/search-results/search-results.component.jsx
@@ -1,12 +1,27 @@
 import React from "react";
 import "./search-results.style.css";
 
-export const SearchResults = ({ searchResults }) => (
+export const SearchResults = ({ searchResults, columns }) => (
 	<div className="results">
-		{searchResults.map((card, i) => (
-			<div key={i} className="result-card" id={card.id}>
-				{card.text}
-			</div>
-		))}
+		{searchResults.length < 1
+			? showNoResults()
+			: showResults(searchResults, columns)}
 	</div>
 );
+
+const showNoResults = () => (
+	<div className="result-card">
+		<span>Could not find any cards, try again.</span>
+	</div>
+);
+
+const showResults = (searchResults, columns) => {
+	return searchResults.map((card, i) => (
+		<div key={i} className="result-card" id={card.id}>
+			<span>{card.text}</span>
+			<span className="search-column">
+				Found in <strong>{columns[card.column_id - 1].title}</strong>
+			</span>
+		</div>
+	));
+};
