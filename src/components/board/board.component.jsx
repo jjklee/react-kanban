b82fc12a3ca80ca96868f0ce64fb0ef6ca7451f9
jjklee <jjlee720@gmail.com>
2019-11-07 17:10:30 -0800

    [fix] unable to find id  with drag and drop

--- src/components/board/board.component.jsx
+++ src/components/board/board.component.jsx
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import Column from "../column/column.component";
 import { DragDropContext } from "react-beautiful-dnd";
 import axios from "axios";
-import "../board/board.style.css";
+import "./board.style.css";
 
-export default class Board extends Component {
+export default class Board extends React.Component {
 	onDragEnd = result => {
-		const { destination, source, draggableId } = result;
-		let columns = this.props.columns;
+		let { destination, source, draggableId } = result;
+		draggableId = parseInt(draggableId);
 		const cardId = parseInt(draggableId);
+		const startColId = parseInt(source.droppableId);
 		if (!destination) return;
 		if (
 			destination.droppableId === source.droppableId &&
@@ -16,42 +17,82 @@ export default class Board extends Component {
 		) {
 			return;
 		}
+		const finishColId = parseInt(destination.droppableId);
+		const start = this.props.columns[parseInt(source.droppableId) - 1];
+		const finish = this.props.columns[parseInt(destination.droppableId) - 1];
+		//moving within the same list
+		if (start === finish) {
+			const newOrder = Array.from(start.card_order);
+			newOrder.splice(source.index, 1);
+			newOrder.splice(destination.index, 0, draggableId);
 
-		const startCol = parseInt(source.droppableId);
-		const finishCol = parseInt(destination.droppableId);
-		if (startCol === finishCol) {
-			let order = columns[startCol - 1].card_order;
-			order.splice(source.index, 1);
-			order.splice(destination.index, 0, cardId);
-			this.props.fetchColumns();
-			this.moveCard(startCol, order);
+			const newColumn = {
+				...start,
+				card_order: newOrder
+			};
+
+			const columns = [...this.props.columns];
+			columns[newColumn.id - 1] = newColumn;
+			this.props.updateColumns(columns);
+			this.moveCard(startColId, newOrder);
 			return;
 		}
 
-		let startOrder = columns[startCol - 1].card_order;
-		let finishOrder = columns[finishCol - 1].card_order;
+		//moving between the list
+		const startOrder = Array.from(start.card_order);
 		startOrder.splice(source.index, 1);
-		finishOrder.splice(destination.index, 0, cardId);
-		columns[startCol - 1].card_order = startOrder;
-		columns[finishCol - 1].card_order = finishOrder;
-		this.props.fetchColumns();
-		this.moveCard(startCol, startOrder);
-		this.moveCard(finishCol, finishOrder);
-		this.updateColumnId(cardId, finishCol);
+
+		const newStart = {
+			...start,
+			card_order: startOrder
+		};
+
+		const finishOrder = Array.from(finish.card_order);
+		finishOrder.splice(destination.index, 0, draggableId);
+
+		const newFinish = {
+			...finish,
+			card_order: finishOrder
+		};
+
+		const columns = [...this.props.columns];
+		columns[newStart.id - 1] = newStart;
+		columns[newFinish.id - 1] = newFinish;
+		this.props.updateColumns(columns);
+		this.moveCardAcrossColumns(
+			startColId,
+			startOrder,
+			finishColId,
+			finishOrder
+		);
+		this.updateColumnId(cardId, destination.droppableId);
 	};
 
 	moveCard = (id, card_order) => {
-		card_order = JSON.stringify(card_order);
 		axios
 			.patch("/api/move", { id, card_order })
-			.then(() => this.props.fetchColumns())
+			.catch(err => console.error("Could not move card, try again."));
+	};
+
+	moveCardAcrossColumns = (
+		startColId,
+		startOrder,
+		finishColId,
+		finishOrder
+	) => {
+		axios
+			.patch("/api/moveColumns", {
+				startColId,
+				startOrder,
+				finishColId,
+				finishOrder
+			})
 			.catch(err => console.error("Could not move card, try again."));
 	};
 
 	updateColumnId = (id, column_id) => {
 		axios
 			.patch("/api/updateCol", { id, column_id })
-			.then(() => this.props.fetchColumns())
 			.catch(err => console.error("Could not update column, try again."));
 	};
 
@@ -60,16 +101,20 @@ export default class Board extends Component {
 			<DragDropContext onDragEnd={this.onDragEnd}>
 				<div className="columns">
 					<div id="overlay"></div>
-					{this.props.columns.map((column, i) => (
-						<Column
-							key={i}
-							column={column}
-							colInd={column.id}
-							fetchColumns={this.props.fetchColumns}
-							idCounter={this.props.idCounter}
-							moveCard={this.moveCard}
-						/>
-					))}
+					{this.props.columns.map(column => {
+						const cards = column.card_order.map(id => this.props.cards[id]);
+						return (
+							<Column
+								key={column.id}
+								column={column}
+								cards={cards}
+								colInd={column.id}
+								fetchColumns={this.props.fetchColumns}
+								idCounter={this.props.idCounter}
+								moveCard={this.moveCard}
+							/>
+						);
+					})}
 				</div>
 			</DragDropContext>
 		);
